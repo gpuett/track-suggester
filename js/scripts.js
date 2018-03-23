f99f38323462bed546dfbacca33b6313d14caff4
Garnett Puett <gpuett@gmail.com>
2018-03-23 13:43:48 -0700

    Make name entry a required field, add gif to error display.

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,9 @@ $(document).ready(function(){
     var q5 = parseInt($("#describe").val());
     var code = $("input:radio[name=code]:checked").val();
     var result = q1 + q2 + q3 + q4 + q5
-      if (q1 === 0 || q2 === 0 || q3 ===0 || q4 === 0 || q5 === 0 || name === "") {
+      if (q1 === 0 || q2 === 0 || q3 ===0 || q4 === 0 || q5 === 0) {
         $(".error").show();
+        $("#jupiter").fadeIn();
     } else if ((result <= 6) && (q3 === 1) && (code === "back") ) {
         $(".android").show();
     } else if ((q4 = 3) || (result > 10 && code === "back")) {
