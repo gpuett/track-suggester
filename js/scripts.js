0f90e7d9aadf980a780b5b6815f72c0d86ea9663
Garnett Puett <gpuett@gmail.com>
2018-03-23 09:59:06 -0700

    Add css styling to center form and tweak various response options.

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,13 @@ $(document).ready(function(){
     var q1 = parseInt($("#develop").val());
     var q2 = parseInt($("#work").val());
     var q3 = parseInt($("#pastime").val());
+    console.log(q3);
     var q4 = parseInt($("#tenyears").val());
     var q5 = parseInt($("#describe").val());
     var result = q1 + q2 + q3 + q4 + q5
-      if (result <= 6 || q3 === 1) {
+      if (result <= 5 && q3 === 1) {
         $(".android").show();
-      } else if (result <=11 || q4 === 2) {
+      } else if (q4 === 2 || result <=10) {
         $(".react").show();
       } else {
         $(".net").show();
