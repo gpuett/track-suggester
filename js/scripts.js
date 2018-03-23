e8be4c58f9836fb561ab361e51d156fc96140d40
Garnett Puett <gpuett@gmail.com>
2018-03-23 14:33:58 -0700

    change conditional for fourth result.

--- js/scripts.js
+++ js/scripts.js
@@ -12,18 +12,19 @@ $(document).ready(function(){
     var code = $("input:radio[name=code]:checked").val();
     console.log(code);
     var result = q1 + q2 + q3 + q4 + q5 + q6
-      if (q1 === 0 || q2 === 0 || q3 ===0 || q4 === 0 || q5 === 0 || q6 === 0) {
-        $(".error").show();
-        $("#jupiter").fadeIn();
+
+    if (q1 === 0 || q2 === 0 || q3 ===0 || q4 === 0 || q5 === 0 || q6 === 0) {
+      $(".error").show();
+      $("#jupiter").fadeIn();
     } else if ((result <= 6) && (q3 === 1) && (code === "back") ) {
-        $(".android").show();
-    } else if (result > 10 && code === "back") {
-        $(".net").show();
+      $(".android").show();
     } else if (result <=10) {
-        $(".react").show();
-    } else {
-        $(".ruby").show();
+      $(".react").show();
+    } else if (result > 10 && code === "back") {
+      $(".net").show();
+    } else if (result > 10 && code === "front") {
+      $(".ruby").show();
     }
-    $(".name").text(name);
+      $(".name").text(name);
   });
 });
