940f1e625af96f66e914a45c2d8c5b035fd719a6
Garnett Puett <gpuett@gmail.com>
2018-03-23 11:50:25 -0700

    Add form for name entry so that responses are directed at user.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
   $("#suggest").submit(function(event){
     event.preventDefault();
     $("#quiz").hide();
+    var name = $("#name").val();
     var q1 = parseInt($("#develop").val());
     var q2 = parseInt($("#work").val());
     var q3 = parseInt($("#pastime").val());
     var q4 = parseInt($("#tenyears").val());
     var q5 = parseInt($("#describe").val());
     var result = q1 + q2 + q3 + q4 + q5
-      if (q1 === 0 || q2 === 0 || q3 ===0 || q4 === 0 || q5 === 0) {
+      if (q1 === 0 || q2 === 0 || q3 ===0 || q4 === 0 || q5 === 0 || name === "") {
         $(".error").show();
     } else if ((result <= 6) && (q3 === 1)) {
         $(".android").show();
@@ -17,5 +18,6 @@ $(document).ready(function(){
     } else if (result <=10) {
         $(".react").show();
     }
+    $(".name").text(name);
   });
 });
