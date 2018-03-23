549c34334bd39b5151a39488b7fb2858095d8259
Garnett Puett <gpuett@gmail.com>
2018-03-23 15:36:53 -0700

    Add result for third answer to radio section.

--- js/scripts.js
+++ js/scripts.js
@@ -10,19 +10,21 @@ $(document).ready(function(){
     var q5 = parseInt($("#describe").val());
     var q6 = parseInt($("#music").val());
     var code = $("input:radio[name=code]:checked").val();
-    console.log(code);
     var result = q1 + q2 + q3 + q4 + q5 + q6
-
+    console.log(code)
     if (q1 === 0 || q2 === 0 || q3 ===0 || q4 === 0 || q5 === 0 || q6 === 0) {
       $(".error").show();
       $("#jupiter").fadeIn();
+    } else if (code === "idk") {
+      $(".idk").show();
+      $("#idk").fadeIn();
     } else if ((result <= 6) && (q3 === 1) && (code === "back") ) {
       $(".android").show();
-    } else if (result <=10) {
+    } else if (result <=12 && code === "front") {
       $(".react").show();
-    } else if (result > 10 && code === "back") {
+    } else if (result > 12 && code === "back") {
       $(".net").show();
-    } else if (result > 10 && code === "front") {
+    } else if (result > 12 && code === "front") {
       $(".ruby").show();
     }
       $(".name").text(name);
