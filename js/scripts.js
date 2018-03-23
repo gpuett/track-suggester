be771a39721ca8edceb7c800232ddc0cc2a6599f
Garnett Puett <gpuett@gmail.com>
2018-03-23 14:27:49 -0700

    add sixth question and fourth possible result.

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,21 @@ $(document).ready(function(){
     var q3 = parseInt($("#pastime").val());
     var q4 = parseInt($("#tenyears").val());
     var q5 = parseInt($("#describe").val());
+    var q6 = parseInt($("#music").val());
     var code = $("input:radio[name=code]:checked").val();
-    var result = q1 + q2 + q3 + q4 + q5
-      if (q1 === 0 || q2 === 0 || q3 ===0 || q4 === 0 || q5 === 0) {
+    console.log(code);
+    var result = q1 + q2 + q3 + q4 + q5 + q6
+      if (q1 === 0 || q2 === 0 || q3 ===0 || q4 === 0 || q5 === 0 || q6 === 0) {
         $(".error").show();
         $("#jupiter").fadeIn();
     } else if ((result <= 6) && (q3 === 1) && (code === "back") ) {
         $(".android").show();
-    } else if ((q4 = 3) || (result > 10 && code === "back")) {
+    } else if (result > 10 && code === "back") {
         $(".net").show();
     } else if (result <=10) {
         $(".react").show();
+    } else {
+        $(".ruby").show();
     }
     $(".name").text(name);
   });
